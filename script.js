d869cc3caf8604c826735e56e2e1336eed9b9554
Manh <07mandao@skola.boras.se>
2024-03-23 20:19:02 +0100

    Upgraded

--- script.js
+++ script.js
@@ -1,41 +1,96 @@
 let words = [];
 let currentWord = 0;
-console.log(words);
-createCards();
-DisplayCard();
+let domDisplay;
+let haveclicked = false;
+let nextButton = document.getElementById("nextCard");
+let lastButton = document.getElementById("lastCard");
+let createButton = document.getElementById("createCard");
+let dialogElement = document.querySelector("dialog");
+let addWordButton = document.getElementById("addBtn")
+let dialogDisplay = document.querySelector("#showWords");
+let dialoglang1 = document.getElementById("language1");
+let dialoglang2 = document.getElementById("language2");
+let finishBtn = document.getElementById("finishBtn");
 
+createButton.addEventListener("click", () => {
+    dialogElement.show();
+});
 
+finishBtn.onclick = function (){
+    dialogElement.close();
+    domDisplay.createCard();
+};
+
+addWordButton.addEventListener("click", () => {
+    if(dialoglang1.value !== "" && dialoglang2.value !== ""){
+        let set = createSet(dialoglang1.value, dialoglang2.value)
+        words.push(set);
+        dialoglang1.value = "";
+        dialoglang2.value = "";
+        domDisplay = DisplayCard();
+    } else {
+        alert("Please enter both words");
+    }
+});
+
+nextButton.addEventListener("click", () => {
+    domDisplay.nextCard();
+});
+
+lastButton.addEventListener("click", () => {
+    domDisplay.lastCard();
+});
 
 function DisplayCard(){
     let card = document.createElement("div");
     card.classList.add("FlashCard");
     let displayText = document.createElement("h1");
     displayText.classList.add("MainDisplay");
-    displayText.innerHTML = words[currentWord]["word1"];
-    card.addEventListener("click", ()=>{
-        if (displayText.innerHTML === words[currentWord]["word1"]){
-            displayText.innerHTML = words[currentWord]["word2"];
-        }
-        else{
-            displayText.innerHTML = words[currentWord]["word1"];
-        }
-    })
-    document.body.appendChild(card);
-    card.appendChild(displayText);
 
-}
-function createCards(){
-    let amountWords = parseInt(prompt("How many word you want to add?"));
-    for(let i = 0; i < amountWords; i++){
-        let word1 = prompt("Enter word1");
-        let word2 = prompt("Enter word2");
-        let pair = createSet(word1, word2);
-        words.push(pair);
+    function createCard(){
+        if(haveclicked === false){
+            displayText.innerHTML = words[currentWord]["word1"];
+            card.addEventListener("click", handleClick);
+            document.body.appendChild(card);
+            card.appendChild(displayText);
+        }
+        haveclicked = true;
     }
+
+    function handleClick() {
+        if(currentWord < words.length && currentWord >= 0){
+            if (displayText.innerHTML === words[currentWord]["word1"]) {
+                displayText.innerHTML = words[currentWord]["word2"];
+            } else {
+                displayText.innerHTML = words[currentWord]["word1"];
+            }
+        }
+    }
+
+    function nextCard(){
+        if (currentWord < words.length - 1){
+            currentWord++;
+            displayText.innerHTML = words[currentWord]["word1"];
+        } else {
+            alert("Trying to reach a word that its not yet defined")
+        }
+    }
+
+    function lastCard(){
+        debugger;
+        if(currentWord > 0){
+            currentWord--;
+            displayText.innerHTML = words[currentWord]["word1"];
+        } else {
+            alert("You have reached the last word!");
+        }
+    }
+
+    return {createCard, nextCard, lastCard}
 }
 
 function createSet (language1, language2){
     let word1 = language1;
     let word2 = language2;
-    return{word1, word2}
+    return {word1, word2};
 }
